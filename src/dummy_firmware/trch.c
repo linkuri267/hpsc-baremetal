745312b1a5e1664b76d9755609867dad594fe1d5
Alexei Colin <acolin@isi.edu>
2018-09-11 12:27:18 -0400

    tests: enable HPPS mailbox test and HPPS boot

--- src/dummy_firmware/trch.c
+++ src/dummy_firmware/trch.c
@@ -10,10 +10,10 @@
 #include "command.h"
 
 #define TEST_RTPS
-// #define TEST_HPPS
+#define TEST_HPPS
 
 #define TEST_FLOAT
-// #define TEST_HPPS_TRCH_MAILBOX
+#define TEST_HPPS_TRCH_MAILBOX
 #define TEST_RTPS_TRCH_MAILBOX
 // #define TEST_IPI
 
