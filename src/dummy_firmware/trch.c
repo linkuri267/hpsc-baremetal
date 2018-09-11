d8e72858764aa2f11b6b8b6a399c564a8a2d38d2
Alexei Colin <acolin@isi.edu>
2018-09-11 12:21:25 -0400

    mailbox; parametrize API for sender and receiver
    Sender and receiver now use same API.

--- src/dummy_firmware/trch.c
+++ src/dummy_firmware/trch.c
@@ -10,11 +10,11 @@
 #include "command.h"
 
 #define TEST_RTPS
-#define TEST_HPPS
+// #define TEST_HPPS
 
 #define TEST_FLOAT
 // #define TEST_HPPS_TRCH_MAILBOX
-// #define TEST_RTPS_TRCH_MAILBOX
+#define TEST_RTPS_TRCH_MAILBOX
 // #define TEST_IPI
 
 int notmain ( void )
@@ -25,14 +25,6 @@ int notmain ( void )
     printf("ENTER PRIVELEGED MODE: svc #0\r\n");
     asm("svc #0");
 
-#ifdef TEST_RTPS
-    reset_component(COMPONENT_RTPS, /* first boot */ true);
-#endif // TEST_RTPS
-
-#ifdef TEST_HPPS
-    reset_component(COMPONENT_HPPS, /* first_boot */ true);
-#endif // TEST_HPPS
-
 #ifdef TEST_FLOAT
     printf("Testing float...\r\n");
     float_test();
@@ -40,28 +32,23 @@ int notmain ( void )
 
 #ifdef TEST_HPPS_TRCH_MAILBOX
     printf("Testing HPPS-TRCH mailbox...\r\n");
-    mbox_init(HPPS_TRCH_MBOX_BASE);
+    mbox_init(HPPS_TRCH_MBOX1_BASE);
     nvic_int_enable(HPPS_TRCH_MBOX_HAVE_DATA_IRQ);
 #endif
 
 #ifdef TEST_RTPS_TRCH_MAILBOX
     printf("Testing RTPS-TRCH mailbox...\r\n");
-    mbox_init(RTPS_TRCH_MBOX_BASE);
+    mbox_init(RTPS_TRCH_MBOX1_BASE);
     nvic_int_enable(RTPS_TRCH_MBOX_HAVE_DATA_IRQ);
+#endif // TEST_RTPS_TRCH_MAILBOX
 
-    /* after some delay */
-    int total = 0;
-    int i, j;
-    for (i = 0; i < 1000000; i++) {
-      for (j = 0; j < 1000; j++)
-        total++;
-    }
-    printf("M4: is the delay long enough?\n");
+#ifdef TEST_RTPS
+    reset_component(COMPONENT_RTPS, /* first boot */ true);
+#endif // TEST_RTPS
 
-    printf("M4: send message to R52\n");
-    for (i = 0; i < 15; i++) 
-       mbox_send(RTPS_TRCH_MBOX_BASE, i);
-#endif // TEST_FLOAT
+#ifdef TEST_HPPS
+    reset_component(COMPONENT_HPPS, /* first_boot */ true);
+#endif // TEST_HPPS
 
 #ifdef TEST_IPI
     printf("Testing IPI...\r\n");
@@ -77,4 +64,18 @@ int notmain ( void )
     while (1) {
         asm("wfi");
     }
+}
+
+// These are in main, not in mailbox.c, because different users of mailbox.c
+// (sender vs. receiver) receive from different indexes. This way mailbox.c
+// can be shared between sender and receiver.
+void mbox_rtps_have_data_isr()
+{
+    uint8_t msg = mbox_have_data_isr(RTPS_TRCH_MBOX1_BASE);
+    cmd_handle(RTPS_TRCH_MBOX0_BASE, msg);
+}
+void mbox_hpps_have_data_isr()
+{
+    uint8_t msg = mbox_have_data_isr(HPPS_TRCH_MBOX1_BASE);
+    cmd_handle(RTPS_TRCH_MBOX0_BASE, msg);
 }
