9f61794f0a87a6557b50c31285d16158db4d9a13
Alexei Colin <acolin@isi.edu>
2018-11-12 18:06:36 -0500

    metrics: print section sizes

---
+++ metrics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+from elftools.elf.elffile import ELFFile
+
+files = sys.argv[1:]
+
+SECTIONS = [
+    '.text',
+    '.bss',
+    '.data',
+    '.rodata',
+]
+
+for fname in files:
+    f = open(fname, "rb")
+    ef = ELFFile(f)
+    print(fname)
+    for s in ef.iter_sections():
+        if s.name in SECTIONS:
+            # data_size not yet available in pyelftools 0.22
+            print("%10s: %4u KB" % (s.name, len(s.data()) / 1024))
+    print
