c897dcc6289ae09a623f819f46871834b776520e
Alexei Colin <acolin@isi.edu>
2018-11-16 17:38:30 -0500

    regops: cast the arg and add variants with base

--- lib/regops.h
+++ lib/regops.h
@@ -4,14 +4,28 @@
 #include <stdint.h>
 #include "printf.h"
 
-#define REG_WRITE32(reg, val) reg_write32(#reg, reg, val)
-#define REG_WRITE64(reg, val) reg_write64(#reg, reg, val)
+#define REG_WRITE32(reg, val) reg_write32(#reg, (volatile uint32_t *)(reg), val)
+#define REG_WRITE64(reg, val) reg_write64(#reg, (volatile uint64_t *)(reg), val)
 
-#define REG_SET32(reg, val) reg_set32(#reg, reg, val)
-#define REG_CLEAR32(reg, val) reg_clear32(#reg, reg, val)
+#define REGB_WRITE32(base, reg, val) reg_write32(#reg, (volatile uint8_t *)(base) + (reg), val)
+#define REGB_WRITE64(base, reg, val) \
+        reg_write64(#reg, (volatile uint64_t *)((volatile uint8_t *)(base) + (reg)), val)
 
-#define REG_READ32(reg) reg_read32(#reg, reg)
-#define REG_READ64(reg) reg_read64(#reg, reg)
+#define REG_SET32(reg, val)   reg_set32(#reg,   (volatile uint32_t *)(reg), val)
+#define REG_CLEAR32(reg, val) reg_clear32(#reg, (volatile uint32_t *)(reg), val)
+
+#define REGB_SET32(base, reg, val) \
+         reg_set32(#reg, (volatile uint32_t *)((volatile uint8_t *)(base) + (reg)), val)
+#define REGB_CLEAR32(base, reg, val) \
+        reg_clear32(#reg, (volatile uint32_t *)((volatile uint8_t *)(base) + (reg)), val)
+
+#define REG_READ32(reg) reg_read32(#reg, (volatile uint32_t *)(reg))
+#define REG_READ64(reg) reg_read64(#reg, (volatile uint32_t *)(reg))
+
+#define REGB_READ32(base, reg) \
+        reg_read32(#reg, (volatile uint32_t *)((volatile uint8_t *)(base) + (reg)))
+#define REGB_READ64(base, reg) \
+        reg_read64(#reg, (volatile uint32_t *)((volatile uint8_t *)(base) + (reg)))
 
 static inline void reg_write32(const char *name, volatile uint32_t *addr, uint32_t val)
 {
