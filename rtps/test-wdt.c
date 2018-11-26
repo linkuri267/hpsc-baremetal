3f6cb3b22893185a807f61797834753515609118
Alexei Colin <acolin@isi.edu>
2018-11-26 18:39:02 -0500

    test: wdt: check that trch has setup the device

--- rtps/test-wdt.c
+++ rtps/test-wdt.c
@@ -31,10 +31,19 @@ static bool check_expiration(unsigned expired_stage, unsigned expected)
 
 int test_wdt()
 {
+    int rc = 1;
+
     gic_int_enable(WDT_PPI_IRQ, GIC_IRQ_TYPE_PPI, GIC_IRQ_CFG_LEVEL);
     volatile unsigned expired_stage = 0;
     wdt = wdt_create_target("RTPS0", WDT_RTPS0_RTPS_BASE,
                             wdt_tick, (void *)&expired_stage);
+    if (!wdt)
+        goto cleanup_irq;
+
+    if (!wdt_is_enabled(wdt)) {
+        printf("ERROR: wdt test: TRCH did not setup the WDT\r\n");
+        goto cleanup;
+    }
 
     unsigned total_timeout = 0;
     unsigned timeouts[] = { wdt_timeout(wdt, 0), wdt_timeout(wdt, 1) };
@@ -54,8 +63,12 @@ int test_wdt()
         runtime += kick_interval;
     }
     if (!check_expiration(expired_stage, 0)) goto cleanup;
+    rc = 0;
 
 cleanup:
     wdt_destroy(wdt);
+    wdt = NULL;
+cleanup_irq:
     gic_int_disable(WDT_PPI_IRQ, GIC_IRQ_TYPE_PPI);
+    return rc;
 }
