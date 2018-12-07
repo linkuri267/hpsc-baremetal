e973a8096b8f8adaf8f0a0a77632643d549b41d8
Alexei Colin <acolin@isi.edu>
2018-12-07 14:17:33 -0500

    reset: toggle the reset and halt GPIOs
    Fixes reset more than the first time.

--- trch/reset.c
+++ trch/reset.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 
 #include "printf.h"
+#include "regops.h"
 
 #include "reset.h"
 
@@ -11,60 +12,41 @@
 #define RPU_CTRL  ((volatile uint8_t *)0xff9a0000)
 
 #define APU__PWRCTL 0x90
-#define APU__PWRCTL__CPUPWRDWNREQ 0x1
+#define APU__PWRCTL__CPUPWRDWNREQ 0xff
 
 #define CRF__RST_FPD_APU 0x104
-#define CRF__RST_FPD_APU__ACPU0_RESET 0x1
+#define CRF__RST_FPD_APU__ACPU0_RESET 0xff
 
 #define CRL__RST_LPD_TOP 0x23c
-#define CRL__RST_LPD_TOP__RPU_R50_RESET 0x1
+#define CRL__RST_LPD_TOP__RPU_R50_RESET 0x3
 
 #define RPU_CTRL__RPU_0_CFG  0x100
 #define RPU_CTRL__RPU_0_CFG__NCPUHALT 0x1
 
-static const char *component_name(component_t c)
-{
-    switch (c) {
-        case COMPONENT_RTPS: return "RTPS";
-        case COMPONENT_HPPS: return "HPPS";
-        default: return "?";
-   }
-}
+#define RPU_CTRL__RPU_1_CFG  0x200
+#define RPU_CTRL__RPU_1_CFG__NCPUHALT 0x1
 
 void reset_component(component_t component)
 {
-    volatile uint32_t *reg_addr;
-    uint32_t reg_val;
-
     switch (component) {
         case COMPONENT_HPPS:
-            reg_addr = (volatile uint32_t *)(APU + APU__PWRCTL);
-            reg_val = APU__PWRCTL__CPUPWRDWNREQ;
+            printf("RESET: HPPS: halt CPU0-7, release CPU0\r\n");
 
-            printf("reset: %s: %p <&- ~0x%08lx\r\n", component_name(component), reg_addr, reg_val);
-            *reg_addr &= ~reg_val;
+            REGB_SET32(APU, APU__PWRCTL, APU__PWRCTL__CPUPWRDWNREQ);
+            REGB_SET32(CRF, CRF__RST_FPD_APU, CRF__RST_FPD_APU__ACPU0_RESET);
 
-            reg_addr = (volatile uint32_t *)(CRF + CRF__RST_FPD_APU);
-            reg_val = CRF__RST_FPD_APU__ACPU0_RESET;
-
-            printf("reset: %s: %p <&- ~0x%08lx\r\n", component_name(component), reg_addr, reg_val);
-            *reg_addr &= ~reg_val;
-
-
+            REGB_CLEAR32(APU, APU__PWRCTL, APU__PWRCTL__CPUPWRDWNREQ & 0x1);
+            REGB_CLEAR32(CRF, CRF__RST_FPD_APU, CRF__RST_FPD_APU__ACPU0_RESET & 0x1);
             break;
         case COMPONENT_RTPS:
-            reg_addr = (volatile uint32_t *)(CRL + CRL__RST_LPD_TOP);
-            reg_val = CRL__RST_LPD_TOP__RPU_R50_RESET;
+            printf("RESET: RTPS: halt CPU0-1, release CPU0\r\n");
 
-            printf("reset: %s: %p <&- ~0x%08lx\r\n", component_name(component), reg_addr, reg_val);
-            *reg_addr &= ~reg_val;
+            REGB_SET32(CRL, CRL__RST_LPD_TOP, CRL__RST_LPD_TOP__RPU_R50_RESET);
+            REGB_CLEAR32(RPU_CTRL, RPU_CTRL__RPU_0_CFG, RPU_CTRL__RPU_0_CFG__NCPUHALT);
+            REGB_CLEAR32(RPU_CTRL, RPU_CTRL__RPU_1_CFG, RPU_CTRL__RPU_1_CFG__NCPUHALT);
 
-            reg_addr = (volatile uint32_t *)(RPU_CTRL + RPU_CTRL__RPU_0_CFG);
-            reg_val = RPU_CTRL__RPU_0_CFG__NCPUHALT;
-
-            printf("clear halt: %s: %p <|- 0x%08lx\r\n", component_name(component), reg_addr, reg_val);
-            *reg_addr |= reg_val;
-
+            REGB_SET32(RPU_CTRL, RPU_CTRL__RPU_0_CFG, RPU_CTRL__RPU_0_CFG__NCPUHALT);
+            REGB_CLEAR32(CRL, CRL__RST_LPD_TOP, CRL__RST_LPD_TOP__RPU_R50_RESET & 0x1);
             break;
    };
 }
