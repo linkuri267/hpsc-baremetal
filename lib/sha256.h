8d965f3cd6bd481ce864940190f70e413598f8fb
David kang <dkang@isi.edu>
2018-12-12 09:02:01 -0800

    SHA256 library is added.

---
+++ lib/sha256.h
@@ -0,0 +1,17 @@
+
+#if !defined(__H_SHA256_H__)
+#define __H_SHA256_H__
+typedef struct mbedtls_sha256_context
+{
+    uint32_t total[2];          /*!< The number of Bytes processed.  */
+    uint32_t state[8];          /*!< The intermediate digest state.  */
+    unsigned char buffer[64];   /*!< The data block being processed. */
+    int is224;                  /*!< Determines which function to use:
+                                     0: Use SHA-256, or 1: Use SHA-224. */
+} mbedtls_sha256_context;
+
+void mbedtls_sha256_init( mbedtls_sha256_context *ctx );
+int mbedtls_sha256_starts_ret( mbedtls_sha256_context *ctx, int is224 );
+int mbedtls_sha256_update_ret( mbedtls_sha256_context *ctx, const unsigned char *input, size_t ilen );
+int mbedtls_sha256_finish_ret( mbedtls_sha256_context *ctx, unsigned char output[32] );
+#endif
