1354861b51465d6b4dec1a0083726c457d65d9d2
Alexei Colin <acolin@isi.edu>
2018-12-13 19:24:30 -0500

    lib: panic: print file and line of assert

--- lib/panic.h
+++ lib/panic.h
@@ -3,7 +3,11 @@
 
 #include "printf.h"
 
-#define ASSERT(cond) if (!(cond)) { panic("ASSERT: " #cond "\r\n");  }
+#define ASSERT(cond) \
+        if (!(cond)) { \
+                printf("ASSERT: %s:%u: " #cond "\r\n", __FILE__, __LINE__); \
+                panic("ASSERT"); \
+        }
 
 void panic(const char *msg);
 
