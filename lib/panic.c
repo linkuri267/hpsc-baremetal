fe19a66a1bef3d1c35c25938ebdec4c13a05245a
Alexei Colin <acolin@isi.edu>
2019-01-02 16:38:07 -0500

    lib: panic: disable interrupts and don't busyloop

--- lib/panic.c
+++ lib/panic.c
@@ -1,11 +1,19 @@
 #include <stdint.h>
 
 #include "printf.h"
+#include "arm.h"
 
 void panic(const char *msg)
 {
+    int_disable();
     printf("PANIC HALT: %s\r\n", msg);
-    while (1); // halt
+
+    // Halt, but without busylooping. Note: even with masked interrupts, WFI
+    // will return on interrupt, but this loop will put us right back to sleep.
+    // Furthermore, once an interrupt is latched, WFI will immediately return
+    // (at least in Qemu), so despite WFI we end up in a busyloop, oh well.
+    while (1)
+        asm ("wfi");
 }
 
 void dump_buf(const char *name, uint32_t *buf, unsigned words)
