6e959fbe5ed1e2020d6ddf0769e2adb074c2d6b7
Alexei Colin <acolin@isi.edu>
2019-01-09 12:43:41 -0500

    trch: subsys: add concept of a CPU Group
    A CPU Group is a lower-level concept than a subsystem. A subsystem
    abastracts the details (e.g. RTPS subsystem may be booted as one OS in
    LOCKSTEP, or as two OSes in SPLIT, or as one OS in SMP).
    
    Most code should be using CPU Group.  Only the very top-level code might
    want to use the subsystem concept, if it desires to present its user
    with a very high-level notion of a subsystem.

--- trch/subsys.c
+++ trch/subsys.c
@@ -2,6 +2,15 @@
 #include "str.h"
 #include "subsys.h"
 
+static const struct cpu_group cpu_groups[] = {
+    [CPU_GROUP_TRCH]       = { SUBSYS_TRCH,     COMP_CPUS_TRCH,     TRCH_NUM_CORES },
+    [CPU_GROUP_RTPS_R52]   = { SUBSYS_RTPS_R52, COMP_CPUS_RTPS_R52, RTPS_R52_NUM_CORES },
+    [CPU_GROUP_RTPS_R52_0] = { SUBSYS_RTPS_R52, COMP_CPU_RTPS_R52_0, 1 },
+    [CPU_GROUP_RTPS_R52_1] = { SUBSYS_RTPS_R52, COMP_CPU_RTPS_R52_1, 1 },
+    [CPU_GROUP_RTPS_A53]   = { SUBSYS_RTPS_A53, COMP_CPUS_RTPS_A53, RTPS_A53_NUM_CORES },
+    [CPU_GROUP_HPPS]       = { SUBSYS_HPPS,     COMP_CPUS_HPPS,     HPPS_NUM_CORES },
+};
+
 static char subsys_name_buf[16];
 
 static const char *subsys_names[] = {
@@ -10,6 +19,12 @@ static const char *subsys_names[] = {
     "RTPS_A53",
     "HPPS",
 };
+
+const struct cpu_group *subsys_cpu_group(enum cpu_group_id id)
+{
+    ASSERT(id < NUM_CPU_GROUPS);
+    return &cpu_groups[id];
+}
 
 const char *subsys_name(subsys_t subsys)
 {
--- trch/subsys.h
+++ trch/subsys.h
@@ -42,6 +42,32 @@ typedef enum { // bitmask
         (COMP_CPU_HPPS_0 | COMP_CPU_HPPS_1 |  COMP_CPU_HPPS_2 | COMP_CPU_HPPS_3 | \
          COMP_CPU_HPPS_4 | COMP_CPU_HPPS_5 |  COMP_CPU_HPPS_6 | COMP_CPU_HPPS_7)
 
+#define TRCH_NUM_CORES     1
+#define RTPS_R52_NUM_CORES 2
+#define RTPS_A53_NUM_CORES 1
+#define HPPS_NUM_CORES     8
+
+#define NUM_CORES \
+      (TRCH_NUM_CORES + RTPS_R52_NUM_CORES + RTPS_A53_NUM_CORES + HPPS_NUM_CORES)
+
+struct cpu_group {
+    subsys_t subsys;
+    comp_t cpu_set;
+    unsigned num_cores;
+};
+
+enum cpu_group_id {
+   CPU_GROUP_TRCH = 0,
+   CPU_GROUP_RTPS_R52,
+   CPU_GROUP_RTPS_R52_0,
+   CPU_GROUP_RTPS_R52_1,
+   CPU_GROUP_RTPS_A53,
+   CPU_GROUP_HPPS,
+   NUM_CPU_GROUPS,
+};
+
+const struct cpu_group *subsys_cpu_group(enum cpu_group_id gid);
+
 const char *subsys_name(subsys_t subsys);
 
 #endif // SUBSYS_H
