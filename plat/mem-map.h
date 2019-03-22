93ac4864bbc173e6622f71a1f9f1ce98788c8e7e
Alexei Colin <acolin@isi.edu>
2019-03-22 18:19:06 -0400

    plat: mem-map: move all regions to first 64Mb

--- plat/mem-map.h
+++ plat/mem-map.h
@@ -5,36 +5,36 @@
 
 // Binary images with executables for HPPS
 #define HPPS_DDR_LOW_ADDR   0x80000000
-#define HPPS_DDR_LOW_SIZE   0x3fe00000 // excludes mem hidden from HPPS
+#define HPPS_DDR_LOW_SIZE   0x3fc00000 // excludes TRCH reservations in HPPS DRAM
 
 // Boot config (TODO: to be replaced by u-boot env/script)
 #define HPPS_BOOT_MODE_ADDR 0x8001fffc
 
 // Shared memory regions accessible to HPPS
-#define HPPS_SHM_ADDR 0xbf800000
+#define HPPS_SHM_ADDR 0x83400000
 #define HPPS_SHM_SIZE   0x400000
 
 // Shared memory regions for userspace
-#define HPPS_SHM_ADDR__TRCH_HPPS 0xbf800000
+#define HPPS_SHM_ADDR__TRCH_HPPS 0x83600000
 #define HPPS_SHM_SIZE__TRCH_HPPS 0x10000
-#define HPPS_SHM_ADDR__HPPS_TRCH 0xbf810000
+#define HPPS_SHM_ADDR__HPPS_TRCH 0x83610000
 #define HPPS_SHM_SIZE__HPPS_TRCH 0x10000
 
 // Shared memory regions for SSW
-#define HPPS_SHM_ADDR__TRCH_HPPS_SSW 0xbfbf0000
+#define HPPS_SHM_ADDR__TRCH_HPPS_SSW 0x839f0000
 #define HPPS_SHM_SIZE__TRCH_HPPS_SSW 0x08000
-#define HPPS_SHM_ADDR__HPPS_TRCH_SSW 0xbfbf8000
+#define HPPS_SHM_ADDR__HPPS_TRCH_SSW 0x839f8000
 #define HPPS_SHM_SIZE__HPPS_TRCH_SSW 0x08000
 
 // Page table should be in memory that is on a bus accessible from the MMUs
 // master port ('dma' prop in MMU node in Qemu DT).  We put it in HPPS DRAM,
 // because that seems to be the only option, judging from high-level Chiplet
 // diagram.
-#define RTPS_HPPS_PT_ADDR                0xbfc00000
-#define RTPS_HPPS_PT_SIZE                  0x200000
+#define RTPS_HPPS_PT_ADDR                0x83e00000
+#define RTPS_HPPS_PT_SIZE                  0x1ff000
 
-#define RT_MMU_TEST_DATA_LO_ADDR         0xbfe00000
-#define RT_MMU_TEST_DATA_LO_SIZE           0x200000
+#define RT_MMU_TEST_DATA_LO_ADDR         0x83fff000
+#define RT_MMU_TEST_DATA_LO_SIZE           0x001000
 
 #define WINDOWS_TO_40BIT_ADDR            0xc0000000
 #define WINDOWS_TO_40BIT_SIZE            0x20000000
