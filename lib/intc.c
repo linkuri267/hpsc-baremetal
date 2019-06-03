e4efa16a55c7ba1ee0402fe7247b4d3de1640d8d
Connor Imes <cimes@isi.edu>
2019-06-03 08:56:55 -0400

    lib: intc: don't assert in intc_disable_all
    This assertion causes an infinite loop if it fails because ASSERT
    ultimately results in a call to intc_disable_all when it fails

--- lib/intc.c
+++ lib/intc.c
@@ -1,4 +1,5 @@
 #include "panic.h"
+#include "printf.h"
 
 #include "intc.h"
 
@@ -23,8 +24,18 @@ void intc_int_disable(struct irq *irq)
 
 void intc_disable_all()
 {
-    ASSERT(intc_ops && intc_ops->disable_all);
-    intc_ops->disable_all();
+    // This function is called from panic(...), and ASSERT can call panic, so
+    // there's a circular dependency which can run infinitely if we ASSERT.
+    // Panic's log message is then buried and the subsystem doesn't halt if
+    // the assertion fails.
+    // ASSERT(intc_ops && intc_ops->disable_all);
+    static int err_once = 0;
+    if (intc_ops && intc_ops->disable_all) {
+        intc_ops->disable_all();
+    } else if (!err_once) {
+        err_once = 1;
+        printf("INTC: WARN: intc_disable_all: intc_ops not configured\r\n");
+    }
 }
 
 unsigned intc_int_num(struct irq *irq)
