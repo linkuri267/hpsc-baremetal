b2302d94b97acd62e45afccb025f830e6aa23833
Alexei Colin <acolin@isi.edu>
2019-08-05 10:28:49 -0400

    lib: regops: extract fields

--- lib/regops.h
+++ lib/regops.h
@@ -4,6 +4,40 @@
 #include <stdint.h>
 #include "panic.h"
 
+#define MAKE_64BIT_MASK(shift, length) \
+    (((~0ULL) >> (64 - (length))) << (shift))
+
+#define REG32(reg, addr)                                              \
+    enum { reg = (addr) };                                            \
+
+#define FIELD(reg, field, shift, length)                              \
+    enum { reg ## __ ## field ## __SHIFT = (shift)};                  \
+    enum { reg ## __ ## field ## __LENGTH = (length)};                \
+    enum { reg ## __ ## field ## __MASK = MAKE_64BIT_MASK(shift, length)};
+
+#define FIELD_EX32(storage, reg, field)                              \
+    extract32((storage), reg ## __ ## field ## __SHIFT,               \
+              reg ## __ ## field ## __LENGTH)
+
+/**
+ * extract32:
+ * @value: the value to extract the bit field from
+ * @start: the lowest bit in the bit field (numbered from 0)
+ * @length: the length of the bit field
+ *
+ * Extract from the 32 bit input @value the bit field specified by the
+ * @start and @length parameters, and return it. The bit field must
+ * lie entirely within the 32 bit word. It is valid to request that
+ * all 32 bits are returned (ie @length 32 and @start 0).
+ *
+ * Returns: the value of the bit field extracted from the input value.
+ */
+static inline uint32_t extract32(uint32_t value, int start, int length)
+{
+    ASSERT(start >= 0 && length > 0 && length <= 32 - start);
+    return (value >> start) & (~0U >> (32 - length));
+}
+
 #define REG_WRITE32(reg, val) reg_write32(#reg, (volatile uint32_t *)(reg), val)
 #define REG_WRITE64(reg, val) reg_write64(#reg, (volatile uint64_t *)(reg), val)
 
