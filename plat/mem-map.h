93304058c74ef9a33bc9d0c9340ce217937186e3
Alexei Colin <acolin@isi.edu>
2019-08-27 16:27:52 -0400

    mem-map: update mem partitioning to Qemu 4
    Synchronize with HPPS u-boot device tree.

--- plat/mem-map.h
+++ plat/mem-map.h
@@ -27,37 +27,37 @@
 #define RTPS_R52_SHM_ADDR__FREE             0x7fc20000
 #define RTPS_R52_DDR_SIZE__FREE             0x003e0000
 
-// Binary images with executables for HPPS
+// Full HPPS DRAM
 #define HPPS_DDR_LOW_ADDR   0x80000000
-#define HPPS_DDR_LOW_SIZE   0x3fc00000 // excludes TRCH reservations in HPPS DRAM
+#define HPPS_DDR_LOW_SIZE   0x40000000
 
 // Boot config (TODO: to be replaced by u-boot env/script)
 #define HPPS_BOOT_MODE_ADDR 0x8001fffc
 
 // Shared memory regions accessible to HPPS
-#define HPPS_SHM_ADDR 0x83400000
+#define HPPS_SHM_ADDR 0x87600000
 #define HPPS_SHM_SIZE   0x400000
 
 // Shared memory regions for userspace
-#define HPPS_SHM_ADDR__TRCH_HPPS 0x83600000
-#define HPPS_SHM_SIZE__TRCH_HPPS 0x10000
-#define HPPS_SHM_ADDR__HPPS_TRCH 0x83610000
-#define HPPS_SHM_SIZE__HPPS_TRCH 0x10000
+#define HPPS_SHM_ADDR__TRCH_HPPS 0x87600000
+#define HPPS_SHM_SIZE__TRCH_HPPS    0x10000
+#define HPPS_SHM_ADDR__HPPS_TRCH 0x87610000
+#define HPPS_SHM_SIZE__HPPS_TRCH    0x10000
 
 // Shared memory regions for SSW
-#define HPPS_SHM_ADDR__TRCH_HPPS_SSW 0x839f0000
-#define HPPS_SHM_SIZE__TRCH_HPPS_SSW 0x08000
-#define HPPS_SHM_ADDR__HPPS_TRCH_SSW 0x839f8000
-#define HPPS_SHM_SIZE__HPPS_TRCH_SSW 0x08000
+#define HPPS_SHM_ADDR__TRCH_HPPS_SSW 0x879f0000
+#define HPPS_SHM_SIZE__TRCH_HPPS_SSW    0x08000
+#define HPPS_SHM_ADDR__HPPS_TRCH_SSW 0x879f8000
+#define HPPS_SHM_SIZE__HPPS_TRCH_SSW    0x08000
 
 // Page table should be in memory that is on a bus accessible from the MMUs
 // master port ('dma' prop in MMU node in Qemu DT).  We put it in HPPS DRAM,
 // because that seems to be the only option, judging from high-level Chiplet
 // diagram.
-#define RTPS_HPPS_PT_ADDR                0x83e00000
+#define RTPS_HPPS_PT_ADDR                0x87e00000
 #define RTPS_HPPS_PT_SIZE                  0x1ff000
 
-#define RT_MMU_TEST_DATA_LO_ADDR         0x83fff000
+#define RT_MMU_TEST_DATA_LO_ADDR         0x87fff000
 #define RT_MMU_TEST_DATA_LO_SIZE           0x001000
 
 #define WINDOWS_TO_40BIT_ADDR            0xc0000000
