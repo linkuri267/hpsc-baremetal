b15e784ea14043310a4b7539cacc9e9591ea520c
Connor Imes <cimes@isi.edu>
2019-09-20 12:13:39 -0400

    lib: shmem: simplify region access - store volatile ptr, not addr

--- lib/shmem.c
+++ lib/shmem.c
@@ -10,7 +10,7 @@
 
 struct shmem {
     struct object obj;
-    volatile void *addr;
+    volatile struct hpsc_shmem_region *shm;
 };
 
 static struct shmem shmems[MAX_SHMEMS] = {0};
@@ -22,7 +22,7 @@ struct shmem *shmem_open(volatile void *addr)
     struct shmem *s = OBJECT_ALLOC(shmems);
     ASSERT(IS_ALIGNED(addr));
     if (s)
-        s->addr = addr;
+        s->shm = (volatile struct hpsc_shmem_region *)addr;
     return s;
 }
 
@@ -33,48 +33,42 @@ void shmem_close(struct shmem *s)
 
 size_t shmem_send(struct shmem *s, void *msg, size_t sz)
 {
-    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
     size_t sz_rem = SHMEM_MSG_SIZE - sz;
     ASSERT(IS_ALIGNED(msg));
     ASSERT(sz <= SHMEM_MSG_SIZE);
-    vmem_cpy(shm->data, msg, sz);
+    vmem_cpy(s->shm->data, msg, sz);
     if (sz_rem)
-        vmem_set(shm->data + sz, 0, sz_rem);
-    shm->status = shm->status | HPSC_SHMEM_STATUS_BIT_NEW;
+        vmem_set(s->shm->data + sz, 0, sz_rem);
+    s->shm->status |= HPSC_SHMEM_STATUS_BIT_NEW;
     return sz;
 }
 
 uint32_t shmem_get_status(struct shmem *s)
 {
-    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
-    return shm->status;
+    return s->shm->status;
 }
 
 bool shmem_is_new(struct shmem *s)
 {
-    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
-    return shm->status & HPSC_SHMEM_STATUS_BIT_NEW;
+    return s->shm->status & HPSC_SHMEM_STATUS_BIT_NEW;
 }
 
 bool shmem_is_ack(struct shmem *s)
 {
-    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
-    return shm->status & HPSC_SHMEM_STATUS_BIT_ACK;
+    return s->shm->status & HPSC_SHMEM_STATUS_BIT_ACK;
 }
 
 void shmem_clear_ack(struct shmem *s)
 {
-    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
-    shm->status &= ~HPSC_SHMEM_STATUS_BIT_ACK;
+    s->shm->status &= ~HPSC_SHMEM_STATUS_BIT_ACK;
 }
 
 size_t shmem_recv(struct shmem *s, void *msg, size_t sz)
 {
-    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
     ASSERT(sz >= SHMEM_MSG_SIZE);
     ASSERT(IS_ALIGNED(msg));
-    mem_vcpy(msg, shm->data, SHMEM_MSG_SIZE);
-    shm->status = shm->status & ~HPSC_SHMEM_STATUS_BIT_NEW; // clear new
-    shm->status = shm->status | HPSC_SHMEM_STATUS_BIT_ACK; // set ACK
+    mem_vcpy(msg, s->shm->data, SHMEM_MSG_SIZE);
+    s->shm->status &= ~HPSC_SHMEM_STATUS_BIT_NEW; // clear new
+    s->shm->status |= HPSC_SHMEM_STATUS_BIT_ACK; // set ACK
     return SHMEM_MSG_SIZE;
 }
