de1624b6ca05a9914250a226a6d607d3ddd8e4b8
Alexei Colin <acolin@isi.edu>
2019-10-11 12:34:51 -0400

    lib: mem: get bzero decl from strings.h
    For consistency with the reste.

--- lib/mem.h
+++ lib/mem.h
@@ -2,14 +2,12 @@
 #define MEM_H
 
 #include <string.h>
-
-void bzero(void *p, size_t size);
+#include <strings.h>
 
 volatile void *vmem_set(volatile void *s, int c, unsigned n);
 
 volatile void *vmem_cpy(volatile void *restrict dest, void *restrict src,
                         unsigned n);
-
 void *mem_vcpy(void *restrict dest, volatile void *restrict src, unsigned n);
 
 #endif // MEM_H
